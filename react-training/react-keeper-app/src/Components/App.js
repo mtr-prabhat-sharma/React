4a48c83651c13353b2c143b26f4b9135590ed3ba
Prabhat Sharma <ps75969@corp.cyient.com>
2024-01-05 17:56:48 +0530

    react keeper app

---
+++ react-training/react-keeper-app/src/Components/App.js
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App1() {
+    return (
+        <div>
+            <Header />
+            <Note />
+            <Footer />
+        </div>
+
+
+    )
+}
+
+export default App1;
---
+++ react-training/react-keeper-app/src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import '../style.css';
+function Header() {
+    return <header>
+        <h1>Keeper</h1>
+    </header>
+}
+
+export default Header;
