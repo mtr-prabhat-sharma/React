4468adc454b693ce240a6227a27bd69c5c633d05
Prabhat Sharma <ps75969@corp.cyient.com>
2024-01-06 11:48:31 +0530

    react keeper app end

--- react-training/react-keeper-app/src/Components/App.js
+++ react-training/react-keeper-app/src/Components/App.js
@@ -1,12 +1,15 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from '../notes';
+
+
 
 function App1() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(noteItem => <Note key={noteItem.key} title={noteItem.title} content={noteItem.content} />)}
             <Footer />
         </div>
 
---
+++ react-training/react-keeper-app/src/notes.js
@@ -0,0 +1,24 @@
+const notes = [
+    {
+        key: 1,
+        title: "Functional Component",
+        content: "A functional component is a simpler way to define a component in React. It is a JavaScript function that accepts props as an argument and returns a React element. Functional components are also known as stateless components because they don't have their own internal state."
+    },
+    {
+        key: 2,
+        title: "Class Component",
+        content: "In React, a class component is a JavaScript class that extends React.Component. Class components were the traditional way of defining components in React before the introduction of functional components and hooks."
+    },
+    {
+        key: 3,
+        title: "Life Cycle Hooks",
+        content: "In class components, React provides a set of lifecycle hooks or methods that allow you to execute code at various stages of a component's lifecycle"
+    },
+    {
+        key: 4,
+        title: "Use State",
+        content: "useState is a React hook that allows functional components to manage state. It is commonly used to introduce stateful behavior into functional components. The useState hook returns an array with two elements:"
+    }
+];
+
+export default notes;
