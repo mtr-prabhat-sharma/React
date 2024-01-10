ea2b7dec0739865a4b2bfda566e584c0ff3f03ff
Prabhat Sharma <ps75969@corp.cyient.com>
2024-01-10 15:37:54 +0530

    React Redux

--- react-training/facebook/src/index.js
+++ react-training/facebook/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import { Login } from './Components/Login'; 
 import reportWebVitals from './reportWebVitals';
-
+import { Container } from './Components/Container';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Container />
   </React.StrictMode>
 );
 
