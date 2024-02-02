01d00825edadef3ffae1abcc40807839761c9485
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2024-02-02 21:23:39 +0530

    useEffect

--- React Hooks/use_effect/src/App.js
+++ React Hooks/use_effect/src/App.js
@@ -1,24 +1,35 @@
+/**
+ * In React, the useEffect hook allows you to perform side effects in functional 
+ * components. Side effects may include data fetching, subscriptions, or manually 
+ * changing the DOM. The useEffect hook runs after every render and replaces 
+ * lifecycle methods such as componentDidMount, componentDidUpdate, and 
+ * componentWillUnmount in class components.
+ */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [second, setSeconds] = useState(0);
+
+  useEffect(() => {
+    document.title = `Seconds: ${second}`;
+
+    const intervalId = setTimeout(() => {
+      setSeconds(prevVal => prevVal + 1);
+    },1000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [second]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Timer: {second} seconds</h2>
+    </>
   );
 }
 
